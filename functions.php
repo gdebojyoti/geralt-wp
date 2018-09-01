6f609c1fb394ddbde84d176c775da07da1725e66
Debojyoti Ghosh <gdebojyoti.mail@gmail.com>
2018-09-02 00:55:51 +0530

    Shortcode functions removed. Created new WP plugin 'Geralt Card Layout'.

--- functions.php
+++ functions.php
@@ -87,42 +87,6 @@ function geralt_settings_menu() {
 add_action( 'admin_menu', 'geralt_settings_menu' );
 
 
-// 3 column layout
-// usage: [one-third]First column text[/one-third][one-third]Second column text[/one-third][one-third]Third column text[/one-third] 
-function geralt_three_col_layout( $atts, $content ) {
-	return '<div class="col-sm-4">' . $content . '</div>';
-}
-add_shortcode( 'one-third', 'geralt_three_col_layout' );
-
-
-// Featured card layout
-// usage: [card-layout-featured category="category title"]
-require_once get_template_directory() . '/inc/card-layout-featured.php'; // Include featured card layout definition
-function geralt_sc_card_featured( $atts = [], $content = null ) {
-	extract( shortcode_atts( array (
-		'category' => '',
-	), $atts ) );
-
-	$category_id = get_cat_ID($category);
-	echo geralt_card_layout_featured($category_id);
-}
-add_shortcode( 'card-layout-featured', 'geralt_sc_card_featured' );
-
-
-// General card layout
-// usage: [card-layout category="category title"]
-require_once get_template_directory() . '/inc/card-layout-general.php'; // Include general card layout definition
-function geralt_sc_card_general( $atts = [], $content = null ) {
-	extract( shortcode_atts( array (
-		'category' => '',
-	), $atts ) );
-
-	$category_id = get_cat_ID($category);
-	echo geralt_card_layout_general($category_id, 'cat');
-}
-add_shortcode( 'card-layout', 'geralt_sc_card_general' );
-
-
 // Theme check recommendation
 function geralt_add_editor_styles() {
 	add_editor_style( 'custom-editor-style.css' );
