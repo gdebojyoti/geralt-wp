f261515b3d9de953a440ed4d6f536f21bd643eca
Debojyoti Ghosh <gdebojyoti.mail@gmail.com>
2018-09-02 16:20:20 +0530

    Nav bar styles updated. CSS refactored.

--- partials/navbar.php
+++ partials/navbar.php
@@ -1,4 +1,4 @@
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
+<nav class="navbar navbar-expand-lg navbar-light">
   <div class="container">
     <a class="navbar-brand" href="/">Geralt</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -6,22 +6,20 @@
     </button>
 
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav mr-auto">
-        <?php
-          wp_nav_menu( array(
-            'theme_location' => 'primary_menu',
-            'depth'      => 2, // 1 = no dropdowns, 2 = with dropdowns.
-            'container'  => false,
-            'menu_class' => 'nav navbar-nav',
-            'fallback_cb' => 'WP_Bootstrap_Navwalker::fallback',
-            'walker' => new WP_Bootstrap_Navwalker()
-          ));
-        ?>
-      </ul>
-      <form class="form-inline my-2 my-lg-0">
+      <?php
+        wp_nav_menu( array(
+          'theme_location' => 'primary_menu',
+          'depth'      => 2, // 1 = no dropdowns, 2 = with dropdowns.
+          'container'  => false,
+          'menu_class' => 'nav navbar-nav ml-auto',
+          'fallback_cb' => 'WP_Bootstrap_Navwalker::fallback',
+          'walker' => new WP_Bootstrap_Navwalker()
+        ));
+      ?>
+      <!-- <form class="form-inline my-2 my-lg-0">
         <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
         <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-      </form>
+      </form> -->
     </div>
   </div>
 </nav>
